4dbbcb4fe79790de78f515a57ec6dbf4dd70eeb6
dicemaster <dicemaster555@gmail.com>
2017-10-30 17:14:36 +0000

    Turned the Player and Wall into classes and temporarily removed the collision system as it needs to be improved.

--- Player_Movement/Sam_movement_collision_test.py
+++ Player_Movement/Sam_movement_collision_test.py
@@ -14,34 +14,43 @@ screen_width = 64 * 15
 screen_height = 64 * 10
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-speed = 8
-player = pygame.image.load('spr_player2.png').convert_alpha()
-pos = Vector2(screen_width / 2 - 32, screen_height / 2 - 32)
-char_width = 64
-char_height = 64
+
+class Player(pygame.sprite.Sprite):
+    width = 64
+    height = 64
+    speed = 8
+    pos = Vector2(screen_width / 2 - 32, screen_height / 2 - 32)
+    player_col = pygame.Rect(pos.x - 4, pos.y - 4, 68, 68)
+    image = pygame.image.load('spr_player2.png').convert_alpha()
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([Player.width, Player.height])
+        self.rect = self.image.get_rect()
 
 
+class Wall:
+    pos = Vector2(128, 128)
+    col = pygame.Rect(pos.x - 4, pos.y - 4, 68, 68)
+    image = pygame.image.load('wall_tile.png').convert_alpha()
+    # def __init__(self):
 
-wall = pygame.image.load('wall_tile.png').convert_alpha()
-wall_pos = Vector2(128, 128)
-wall_col = pygame.Rect(wall_pos.x-4, wall_pos.y-4, 68, 68)
 
-
-def player_movement():
+def player_controller():
     # variable for key press
     pressed_keys = pygame.key.get_pressed()
 
     # horizontal movement
-    if pressed_keys[K_a] and player_col.colliderect(wall_col) == False:
-        pos.x -= speed
-    if pressed_keys[K_d]and player_col.colliderect(wall_col) == False:
-        pos.x += speed
+    if pressed_keys[K_a]:
+        Player.pos.x -= Player.speed
+    if pressed_keys[K_d]:
+        Player.pos.x += Player.speed
 
     # vertical movement
-    if pressed_keys[K_w]and player_col.colliderect(wall_col) == False:
-        pos.y -= speed
+    if pressed_keys[K_w]:
+        Player.pos.y -= Player.speed
     if pressed_keys[K_s]:
-        pos.y += speed
+        Player.pos.y += Player.speed
 
 # game while loop
 running = True
@@ -52,24 +61,23 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
 
-    player_col = pygame.Rect(pos.x - 4, pos.y - 4, 68, 68)
-    player_movement()
+    player_controller()
 
     # horizontal boundaries
-    if pos.x > screen_width - char_width:
-        pos.x = screen_width - char_width
-    elif pos.x < 0:
-        pos.x = 0
+    if Player.pos.x > screen_width - Player.width:
+        Player.pos.x = screen_width - Player.width
+    elif Player.pos.x < 0:
+        Player.pos.x = 0
     # vertical boundaries
-    if pos.y > screen_height - char_height:
-        pos.y = screen_height - char_height
-    elif pos.y < 0:
-        pos.y = 0
+    if Player.pos.y > screen_height - Player.height:
+        Player.pos.y = screen_height - Player.height
+    elif Player.pos.y < 0:
+        Player.pos.y = 0
 
     # window creation and player blitting
     screen.fill((135, 135, 135))
-    screen.blit(player, pos)
-    screen.blit(wall, (wall_pos.x, wall_pos.y))
+    screen.blit(Player.image, Player.pos)
+    screen.blit(Wall.image, (Wall.pos.x, Wall.pos.y))
 
     pygame.display.update()
     fpsClock.tick(FPS)
